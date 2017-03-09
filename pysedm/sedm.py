0cb9bb327737ea83e13107d1bc932e586d614482
Mickael <mick.rigault@gmail.com>
2017-03-09 17:41:36 +0100

    tools kpy to pyifu

--- pysedm/sedm.py
+++ pysedm/sedm.py
@@ -10,5 +10,60 @@ from pyifu.spectroscopy import Cube
 SEDMSPAXELS = np.asarray([[ np.sqrt(3.)/2., 1./2],[0, 1],[-np.sqrt(3.)/2., 1./2],
                           [-np.sqrt(3.)/2.,-1./2],[0,-1],[ np.sqrt(3.)/2.,-1./2]])*2/3.
 
+def kpy_to_e3d(filename, lbda, savefile=None):
+    """ Converts SEDmachine kpy .npy data into pyifu e3d cubes. 
+    (rotation information missing)
+    
+    Parameters
+    ----------
+    filename: [string]
+        .npy data created using the kpy software
+
+    lbda: [array]
+        wavelength array of the cube.
+
+    savefile: [string/None] -optional-
+        if you want to save the cube, provide here its fullpath
+    
+    Returns
+    -------
+    pyifu Cube 
+    """
+    from scipy.interpolate import interp1d
+    # 
+    #  get the data
+    data, header = np.load(filename)
+    # 
+    # empty the cube
+    cube = Cube(None)
+    cubeflux     = []
+    spaxel_map   = {}
+    for i, ext in enumerate(data):
+        try:
+            lbda_nm, flux_ = ext.get_flambda("spec")
+            cubeflux.append(interp1d(lbda_nm*10, flux_, kind="cubic")(lbda))
+            spaxel_map[i] = np.asarray([ext.X_as,ext.Y_as]*np.sqrt(3))
+        except:
+            # will crash if given lbda is outside the Extraction wavelength
+            continue
+        
+    # create it
+    cube.create(np.asarray(cubeflux).T,lbda=lbda, spaxel_mapping=spaxel_map, variance=None)
+    cube.set_spaxel_vertices(SEDMSPAXELS/ np.sqrt(3))
+    for k,v in header["header"].items():
+        cube.header[k] = v
+
+    # returns it.
+    if savefile is not None:
+        cube.writeto(savefile)
+        
+    return cube
+
+
+#################################
+#                               #
+#    SEDMachine Cube            #
+#                               #
+################################# 
 class SEDMCube( Cube ):
     """ SEDM Cube """

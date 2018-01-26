e63213fa6d4f3a60731d8c018bc66e6630f2831d
MickaelRigault <mick.rigault@gmail.com>
2018-01-26 17:25:40 +0100

    Now ability to merge subprocesses of WaveSolutions

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ MAINTAINER_EMAIL = 'mickael.rigault@clermont.in2p3.fr'
 URL = 'https://github.com/MickaelRigault/pysedm/'
 LICENSE = 'BSD (3-clause)'
 DOWNLOAD_URL = 'https://github.com/MickaelRigault/pysedm/tarball/0.6'
-VERSION = '0.6.7'
+VERSION = '0.6.8'
 
 try:
     from setuptools import setup, find_packages

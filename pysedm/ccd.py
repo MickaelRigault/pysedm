fe511a72053b743fd49b06844fc0b7262e0f90d9
MickaelRigault <mick.rigault@gmail.com>
2018-05-29 11:08:24 +0200

    Code stability improved on Extract_Spectrum

--- pysedm/ccd.py
+++ pysedm/ccd.py
@@ -605,10 +605,15 @@ class CCD( BaseCCD ):
         # MultiProcess #
         # ------------ #
         def _build_ith_flux_(i_):
-                
-            lbda_, flux_, variance_ = self.extract_spectrum(i_, wavesolution, lbda=lbda,
+            try:
+                lbda_, flux_, variance_ = self.extract_spectrum(i_, wavesolution, lbda=lbda,
                                                                 get_spectrum=False,
                                                                 pixel_shift=pixel_shift)
+            except:
+                warnings.warn("FAILING EXTRACT_SPECTRUM for trace index %d: most likely wavesolution failed for this trace. *NaN Spectrum set*"%i_)
+                flux_ = np.ones(len(lbda) )*np.NaN
+                variance_ = np.ones(len(lbda) )*np.inf
+                
             cubeflux_[i_] = flux_
             if cubevar_ is not None:
                 cubevar_[i_] = variance_

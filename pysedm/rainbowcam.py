b175826732792fae75d871060e07af3d809ea39c
Don Neill <neilljd@gmail.com>
2019-11-08 10:43:19 -0800

    re-scales stacked guider image so counts are roughly equal to original ADU, now checks for SEDMPY environment variable to find do_astrom script before using default path, PEP8 code cleanup.

--- pysedm/rainbowcam.py
+++ pysedm/rainbowcam.py
@@ -1,9 +1,7 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
 """ Access the rainbow camera images """
-
 #
 # This method is an a
 #
@@ -13,7 +11,9 @@ from astropy import time
 from astropy.io import fits
 from . import io
 RAINBOW_DATA_SOURCE = "/scr2/sedm/raw/"
-#READOUT_NOISE       = 4
+SEDMPY_CODE_PATH = "/scr2/sedmdrp/sedmpy/"
+# READOUT_NOISE       = 4
+
 
 # ================== #
 #  Main Function     #
@@ -31,14 +31,18 @@ def build_meta_ifu_guider(ifufile, outdir=None, solve_wcs=True, verbose=False):
     Parameters
     ----------
     ifufile: [string]
-        Path of a ifu .fits (or derived .fits as long as they contain the basic associated header keywords)
+        Path of a ifu .fits (or derived .fits as long as they contain
+        the basic associated header keywords)
        
-    outfir: [string] -optional-
+    outdir: [string] -optional-
         Where does the guider image should be saved. 
         If None, it will be in the same directory as the `infufile`
         
     solve_wcs: [bool] -optional-
         Shall "do_astro" (based on astrometry.net) be ran on the stacked image?
+
+    verbose: [bool] -optional-
+        Extra output
         
     Returns
     -------
@@ -47,19 +51,20 @@ def build_meta_ifu_guider(ifufile, outdir=None, solve_wcs=True, verbose=False):
     savefile = build_stacked_guider(ifufile, outdir)
     if savefile:
         if verbose:
-            print(" guider image built for %s"%ifufile)
+            print(" guider image built for %s" % ifufile)
             print(savefile)
         if solve_wcs:
             if verbose:
-                print(" running astrometry on %s"%savefile)
+                print(" running astrometry on %s" % savefile)
             run_do_astrom(savefile)
-            if not os.path.isfile( savefile.replace( ".fits", "_astrom.fits" ) ):
+            if not os.path.isfile(savefile.replace(".fits", "_astrom.fits")):
                 print("do_astrom has failed.")
             else:
                 print("do_astrom succeeded.")
     else:
         print("ERROR - unable to build guider image")
-            
+
+
 # ================== #
 #   Function         #
 # ================== #
@@ -75,30 +80,36 @@ def build_stacked_guider(ifufile, outdir=None, overwrite=True):
     Parameters
     ----------
     ifufile: [string]
-        Path of a ifu .fits (or derived .fits as long as they contain the basic associated header keywords)
+        Path of a ifu .fits (or derived .fits as long as they contain
+         the basic associated header keywords)
        
-    outfir: [string] -optional-
+    outdir: [string] -optional-
         Where does the guider image should be saved. 
         If None, it will be in the same directory as the `infufile`
 
+    overwrite: [bool] -optional-
+        Set to overwrite existing file
+
     """
-    guiders = get_ifu_guider_images( ifufile )
-    stacked_image, nstack = stack_images( guiders )
+    guiders = get_ifu_guider_images(ifufile)
+    stacked_image, nstack, avscl = stack_images(guiders)
     # did we get any?
     if nstack < 1:
         print("ERROR - no guider images found")
         return None
     # - building the .fits
-    date = io.header_to_date( fits.getheader(ifufile) )
+    date = io.header_to_date(fits.getheader(ifufile))
     filein = ifufile.split("/")[-1]
     if outdir is None:
         outdir = io.get_datapath(date)
         
     savefile = outdir+"/guider_%s"%filein
-    hdulist = fits.HDUList([fits.PrimaryHDU(stacked_image, fits.getheader(guiders[0]))])
+    hdulist = fits.HDUList([fits.PrimaryHDU(stacked_image,
+                                            fits.getheader(guiders[0]))])
     hdulist[0].header['NSTACK'] = nstack
     hdulist[0].header['STACKMTH'] = 'median'
     hdulist[0].header['SCALEMTH'] = 'median'
+    hdulist[0].header['STACKSCL'] = avscl
     hdulist.writeto(savefile, overwrite=overwrite)
     return savefile
 
@@ -106,12 +117,17 @@ def build_stacked_guider(ifufile, outdir=None, overwrite=True):
 def run_do_astrom(guider_filename_fullpath):
     """ Run the do_astrom script in /scr2/sedmdrp/bin
     """
+    try:
+        do_astrom = os.path.join(os.environ['SEDMPY'], 'bin/do_astrom')
+    except KeyError:
+        do_astrom = SEDMPY_CODE_PATH + 'bin/do_astrom'
     import subprocess
-    cmd = ["/scr2/sedmdrp/bin/do_astrom", guider_filename_fullpath]
+    cmd = [do_astrom, guider_filename_fullpath]
     print(" ".join(cmd))
     subprocess.call(cmd)
     # Test results
-    astrom_output = guider_filename_fullpath.replace(".fits", "_astrom.fits").replace(".gz", "")
+    astrom_output = guider_filename_fullpath.replace(
+        ".fits", "_astrom.fits").replace(".gz", "")
     if not os.path.exists(astrom_output):
         print("ERROR - astrometry failed, trying a median subtraction")
         from scipy import ndimage
@@ -131,9 +147,13 @@ def run_do_astrom(guider_filename_fullpath):
 #   Tools            #
 # ================== #
 
-def get_rainbow_datapath(DATE):
+def get_rainbow_datapath(ut_date):
     """ returns the path of the rainbow camera data """
-    return RAINBOW_DATA_SOURCE+DATE+"/"
+    try:
+        source = os.environ['SEDMRAWPATH']
+    except KeyError:
+        source = RAINBOW_DATA_SOURCE
+    return os.path.join(source, ut_date) + "/"
 
 
 def get_ifu_guider_images(ifufile):
@@ -180,20 +200,25 @@ def stack_images(rainbow_files, method="median", scale="median"):
     """ return a 2D image corresponding of the stack of the given data """
     # - 
     if scale not in ['median']:
-        raise NotImplementedError("only median scaling implemented (not %s)"%scale)
+        raise NotImplementedError("only median scaling implemented (not %s)"
+                                  % scale)
     
     if method not in ['median']:
-        raise NotImplementedError("only median stacking method implemented (not %s)"%method)
+        raise NotImplementedError(
+            "only median stacking method implemented (not %s)" % method)
     # Load the normalized data
 
     datas = []
+    scales = []
+    scaling = 1.
+    avscale = 1.
     for f_ in rainbow_files:
-        data_   = fits.getdata(f_)
-        # header_ = fits.getheader(f_) all have the same gain and readout noise...
+        data_ = fits.getdata(f_)
+        # header_= fits.getheader(f_) all have the same gain and readout noise
         if scale in ["median"]: 
             scaling = np.median(data_)
+            scales.append(scaling)
         datas.append(data_/scaling)
+        avscale = np.mean(scales)
 
-    return np.median(datas, axis=0), len(datas)
-
-
+    return np.median(datas, axis=0)*avscale, len(datas), avscale

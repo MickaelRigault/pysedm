e016deddbfe7232e3b58d47ecb4aa693bf32ae6c
Young-Lo <47598333+Young-Lo@users.noreply.github.com>
2020-05-05 14:00:08 +0200

    setup.py: version to 0.27.0 for contsep module.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ MAINTAINER_EMAIL = 'mickael.rigault@clermont.in2p3.fr'
 URL = 'https://github.com/MickaelRigault/pysedm/'
 LICENSE = 'BSD (3-clause)'
 DOWNLOAD_URL = 'https://github.com/MickaelRigault/pysedm/tarball/0.26'
-VERSION = '0.26.2'
+VERSION = '0.27.0'
 
 try:
     from setuptools import setup, find_packages

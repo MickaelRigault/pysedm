4139d1fafd1d84fa981184a77366e991f378cb4a
Yashvi Sharma <yashuvatsas@gmail.com>
2022-11-26 05:21:47 -0700

    for debugging

--- pysedm/ccd.py
+++ pysedm/ccd.py
@@ -120,7 +120,9 @@ def get_dome(domefile, tracematch=None,  load_sep=False, **kwargs):
     sep.set_extract_pixstack(3000000)
     if load_sep:
         dome.datadet = dome.data/np.sqrt(np.abs(dome.data))
-        dome.sep_extract(thresh=dome._get_sep_extract_threshold_(), on="datadet")
+        dome.sep_extract(thresh=50, on="datadet")
+        fig = dome.show()
+        dome.display_sepobjects(fig['ax'])
 
     if tracematch is not None:
         # = Tracematch that gonna help the background
